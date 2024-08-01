2982cd80201c8366da6789d2f17985a906c76985
Joanne Wendoh <joannewendoh.com>
2024-08-01 18:21:51 +0300

    complete

--- RecruitConnect/src/components/Jobcard.jsx
+++ RecruitConnect/src/components/Jobcard.jsx
@@ -1,37 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import '../jobcard.css';
 
 const JobCard = ({ job, onClick, detailed }) => {
-  const [employerName, setEmployerName] = useState('');
-
-  useEffect(() => {
-    const fetchEmployerName = async () => {
-      try {
-        const response = await fetch(`http://127.0.0.1:5000/employers/${job.employer_id}`, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-          }
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch employer name');
-        }
-
-        const data = await response.json();
-        setEmployerName(data.name);
-      } catch (error) {
-        console.error('Error fetching employer name:', error);
-      }
-    };
-
-    fetchEmployerName();
-  }, [job.employer_id]);
-
   return (
     <div className={`job-card ${detailed ? 'detailed' : ''}`} onClick={onClick}>
       <h2>{job.title}</h2>
-      <p><strong>Employer:</strong> {employerName}</p>
       <p><strong>Company Email:</strong> {job.company_email}</p>
       <p><strong>Location:</strong> {job.location}</p>
       {detailed && (
