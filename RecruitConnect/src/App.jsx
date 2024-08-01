f71780e64fcafe5f7eee91dd56843aa81f70147b
shukri <didashukri0@gmail.com>
2024-08-01 23:12:32 +0300

    complete searchbar

--- RecruitConnect/src/App.jsx
+++ RecruitConnect/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Landing from './components/Landing';
-import Search from './components/Search';
 import Joblist from './components/Joblist';
 import Footer from './components/Footer';
 import EmployerLogin from './components/Employerlogin';
@@ -19,9 +18,7 @@ const App = () => {
           <Route>
         
               <Route path="/" element={<Landing />}></Route>
-              <Route path="/search" element={<Search />}></Route>
               <Route path="/joblist" element={<Joblist />} />
-              <Route path="/jobs" element={<Landing />}></Route>
               <Route path="/employer-login" element={<EmployerLogin />}></Route>
               <Route path="/seeker-login" element={<SeekerLogin />}></Route>
        
