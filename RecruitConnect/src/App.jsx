d81d441743f36d939cb0e28f6581a21e7e12507d
shukri <didashukri0@gmail.com>
2024-08-03 21:48:24 +0300

    complete

--- RecruitConnect/src/App.jsx
+++ RecruitConnect/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Landing from './components/Landing';
 import Search from './components/Search';
-import JobList from './components/Joblist';
+import Joblist from './components/Joblist';
 import Footer from './components/Footer';
 import EmployerLogin from './components/Employerlogin';
 import SeekerLogin from './components/Seekerlogin';
