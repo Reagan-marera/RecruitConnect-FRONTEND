a2e54df4e3b38e5c3e78604fc0a8c52495d4e35f
Cheasy Jay <jamohgamer@gmail.com>
2024-08-04 15:01:56 +0300

    profile

--- RecruitConnect/src/seekercomponents/JobSeeker.jsx
+++ RecruitConnect/src/seekercomponents/JobSeeker.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Application from './Application';
+import Profile from './Profile';
 import './seeker.css'; 
 
 function JobSeeker() {
@@ -10,11 +11,13 @@ function JobSeeker() {
         <ul className="jobseeker-nav-list">
           <li className="jobseeker-nav-item">
             <Link to="application" className="jobseeker-nav-button">Application</Link>
+            <Link to="profile" className="jobseeker-nav-button">Profile</Link>
           </li>
         </ul>
       </nav>
       <Routes>
-        <Route path="application" element={<Application />} />
+      <Route path="application" element={<Application />} />
+      <Route path="profile" element={<Profile />} />
       </Routes>
     </div>
   );
