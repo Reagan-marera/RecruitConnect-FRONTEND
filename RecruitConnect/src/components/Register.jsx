ee7a444089cb407109294688d174316a9f51d007
Cheasy Jay <jamohgamer@gmail.com>
2024-08-04 17:20:10 +0300

    this should be the correct register form

--- RecruitConnect/src/components/Register.jsx
+++ RecruitConnect/src/components/Register.jsx
@@ -9,7 +9,7 @@ const Register = () => {
     email: "",
     password: "",
     confirmPassword: "",
-    role: "user" // Default role
+    role: "user" 
   });
 
   const [error, setError] = useState("");
