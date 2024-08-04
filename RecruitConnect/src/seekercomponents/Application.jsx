7a139fcb65d5bfa724118cb29fc488c02fc0a9e2
Cheasy Jay <jamohgamer@gmail.com>
2024-08-04 14:56:42 +0300

    Styling the application page

--- RecruitConnect/src/seekercomponents/Application.jsx
+++ RecruitConnect/src/seekercomponents/Application.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Application.css'; 
 
 const Application = () => {
   return (
-    <div>
-      <h1>Application page.</h1>
-      <p>You do not have any job applications yet</p>
-      <button onClick={() => alert("find your dream job")}>Find Dream Job</button>
+    <div className="card">
+      <div className="card-header">
+        Application page.
+      </div>
+      <div className="card-body">
+        <p>You do not have any job applications yet</p>
+        <button className="find-job-button" onClick={() => alert("Find your dream job")}>Find Dream Job</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Application
+export default Application;
