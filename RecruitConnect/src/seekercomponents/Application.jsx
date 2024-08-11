6bcc35827be58d8aecda7c8072404b577eb62168
Cheasy Jay <jamohgamer@gmail.com>
2024-08-11 15:30:45 +0300

    Featuring jobseeker dash styling

--- RecruitConnect/src/seekercomponents/Application.jsx
+++ RecruitConnect/src/seekercomponents/Application.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import './Application.css'; 
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 const Application = () => {
   const navigate = useNavigate();
-  
 
-  const handleFindJobClick = () =>{
-    toast("Wow, this is a toast notification!");
+  const handleFindJobClick = () => {
     navigate('/joblist');
   }
+
   return (
-    <div className="card">
-      <div className="card-header">
-        Application page.
+    <div className="application-container">
+      <div className="application-header">
+        <code>Application page.</code>
       </div>
-      <div className="card-body">
+      <div className="application-body">
+        <p className="application-info">
+          Welcome to your job application tracker! Here you can manage all your job applications in one place.
+        </p>
+        <p className="application-info">
+          Ready to start your job search? Click the button below to explore the latest job opportunities tailored just for you.
+        </p>
         <button className="find-job-button" onClick={handleFindJobClick}>Find Dream Job</button>
       </div>
     </div>
