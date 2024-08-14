31a301898a125399eda6f76d02a03cc4950a2d2b
Cheasy Jay <jamohgamer@gmail.com>
2024-08-14 13:00:59 +0300

    Featuring application styling

--- RecruitConnect/src/seekercomponents/Application.jsx
+++ RecruitConnect/src/seekercomponents/Application.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Application.css'; 
 import { useNavigate } from 'react-router-dom';
+import image from './images/image.png'
+
 
 const Application = () => {
   const navigate = useNavigate();
@@ -16,13 +18,14 @@ const Application = () => {
       </div>
       <div className="application-body">
         <p className="application-info">
-          Welcome to your job application tracker! Here you can manage all your job applications in one place.
+          Welcome to Recruit Connect your job application tracker! Here you can manage all your job applications in one place.
         </p>
         <p className="application-info">
           Ready to start your job search? Click the button below to explore the latest job opportunities tailored just for you.
         </p>
         <button className="find-job-button" onClick={handleFindJobClick}>Find Dream Job</button>
       </div>
+         <img src={image} alt="Networking"/>  
     </div>
   );
 };
